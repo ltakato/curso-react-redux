99d333adbe6a14e47b8610c5a01484e9d5203fac
takato <leonardo.takato@soaexpert.com.br>
2019-02-21 00:58:17 -0300

    feat: add new files of babel loader lesson

---
+++ ex.2/index.js
@@ -0,0 +1,4 @@
+import Pessoa from './pessoa';
+
+const pessoa = new Pessoa('Capivara San');
+console.log(pessoa.toString());
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,17 @@ module.exports = {
     devServer: {
         port: 8080,
         contentBase: './public'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 };
